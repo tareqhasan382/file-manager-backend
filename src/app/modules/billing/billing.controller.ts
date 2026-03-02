8244fca25f97daadd771c16849a396455c6909ea
unknown <tareqhasan382@gmail.com>
2026-03-03 00:32:19 +0600

    fix mismatch route name

--- src/app/modules/billing/billing.controller.ts
+++ src/app/modules/billing/billing.controller.ts
@@ -2,13 +2,18 @@ import { Request, Response } from "express";
 import httpStatus from "http-status";
 import catchAsync from "../../../shared/catchAsync";
 import { billingServices } from "./billing.service";
+import { sendEmail } from "../../../lib/email";
 
 const create = catchAsync(async (req: Request, res: Response) => {
   const data = req.body;
   const { userId, tenantId } = req.user as any;
 
   const result = await billingServices.createBilling({ plan:data?.plan, userId });
-
+  await sendEmail({
+    to: "tareqhasan382@gmail.com",
+    subject: "Stripe checkout session created",
+    text: `Hi Tareq, your subscription will renew on .`,
+  });
   res.status(httpStatus.OK).json({
     success: true,
     message: "Stripe checkout session created",
