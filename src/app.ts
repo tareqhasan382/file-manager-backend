b68e3345010f687b888c81a4d134ee32fe1d8501
unknown <tareqhasan382@gmail.com>
2026-03-03 06:07:05 +0600

    cc

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 
 /* -------------------- CORS -------------------- */
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000", "https://file-manager-frontend-lyart.vercel.app"],
+  origin: ["http://localhost:5173", "http://localhost:3000", "https://file-manager-frontend-lyart.vercel.app","*"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
